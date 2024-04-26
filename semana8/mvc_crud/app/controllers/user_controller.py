f85b2b5c9f4a46637d2fa72f55e91401a75d6dc5
CristianJesusApazaSarmiento <apazacristian044@gmail.com>
2024-04-25 21:31:56 -0400

    Avance 24/04/2024

---
+++ semana8/mvc_crud/app/controllers/user_controller.py
@@ -0,0 +1,62 @@
+from flask import Blueprint, request, redirect, url_for
+
+from views import user_view
+
+from models.user_model import User
+
+user_bp = Blueprint('user', __name__)
+
+@user_bp.route('/')
+def usuarios():
+    users = User.get_all()
+    return user_view.usuarios(users)
+
+@user_bp.route('/users', methods=['GET', 'POST'])
+def registro():
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email = request.form['email']
+        password = request.form['password']
+        birthdate = request.form['birthdate']
+        
+        user = User(first_name, last_name, email, password, birthdate)
+        user.save()
+        
+        return redirect(url_for('user.usuarios'))
+    return user_view.registro()
+
+@user_bp.route("/users/<int:id>", methods=["GET"])
+def obtener_usuario(id):
+    user = User.get_by_id(id)
+    if not user:
+        return "Usuario no encontrado", 404
+    return user_view.actualizar(user)
+
+@user_bp.route("/users/<int:id>", methods=["POST"])
+def actualizar(id):
+    user = User.get_by_id(id)
+    if not user:
+        return "Usuario no encontrado", 404
+    first_name = request.form["first_name"]
+    last_name = request.form["last_name"]
+    email = request.form["email"]
+    password = request.form["password"]
+    birthdate = request.form["birthdate"]
+    
+    user.first_name = first_name
+    user.last_name = last_name
+    user.email = email
+    user.password = password
+    user.birthdate = birthdate
+    
+    user.update()
+    return redirect(url_for("user.usuarios"))
+
+@user_bp.route("/users/<int:id>/delete", methods=["POST"])
+def eliminar(id):
+    user = User.get_by_id(id)
+    if not user:
+        return "Usuario no encontrado", 404
+    user.delete()
+    return redirect(url_for("user.usuarios"))
