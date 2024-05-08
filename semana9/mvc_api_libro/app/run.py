b4a95eb73bf0c7dc986ff40402caace82167907e
CristianJesusApazaSarmiento <apazacristian044@gmail.com>
2024-05-08 14:12:48 -0400

    Avance 08/05/2024

--- semana9/mvc_api_libro/app/run.py
+++ semana9/mvc_api_libro/app/run.py
@@ -1,8 +1,16 @@
 from flask import Flask
 from controllers.libro_controller import libro_bp
+from flask_swagger_ui import get_swaggerui_blueprint
 from database import db
 
 app = Flask(__name__)
+
+SWAGGER_URL = "/api/docs" 
+API_URL = "/static/swagger.json" 
+swagger_ui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL, API_URL, config={"app_name": "Biblioteca API"}
+)
+app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///biblioteca.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
